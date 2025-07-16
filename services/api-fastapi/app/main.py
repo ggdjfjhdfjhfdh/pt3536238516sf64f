88c4464756bf22481d48590e077f95c89492757b
ggdjfjhdfjhfdh <chicagogravity@pm.me>
2025-07-16 14:43:53 +0200

    first455

--- services/api-fastapi/app/main.py
+++ services/api-fastapi/app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # CORS – tu dominio de Vercel
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://www.auditatetumismo.es/"],
+    allow_origins=["https://www.auditatetumismo.es"],
     allow_methods=["POST", "OPTIONS"],
     allow_headers=["*"],
     allow_credentials=True,
