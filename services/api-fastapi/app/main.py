5adcac1ff2c045b92df5a8579adb3c933b129d46
ggdjfjhdfjhfdh <chicagogravity@pm.me>
2025-07-16 12:27:13 +0200

    first455

--- services/api-fastapi/app/main.py
+++ services/api-fastapi/app/main.py
@@ -7,9 +7,10 @@ app = FastAPI()
 # CORS – tu dominio de Vercel
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://pt-ldn-61169603-jpga.vercel.app/"],
-    allow_methods=["POST, OPTIONS"],
+    allow_origins=["https://pt-ldn-61169603-jpga.vercel.app"],
+    allow_methods=["POST", "OPTIONS"],
     allow_headers=["*"],
+    allow_credentials=True,
 )
 
 # ---------- Stripe ----------
