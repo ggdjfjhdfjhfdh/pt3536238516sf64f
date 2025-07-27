6005c38625ad9aab009710455c2a68969e8b2b30
ggdjfjhdfjhfdh <chicagogravity@pm.me>
2025-07-27 12:55:28 +0200

    first455

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     package_data={
-        'pentest': ['mitre_mapping.yaml'],
-        '': ['templates/*.html'],
+        'pentest': ['mitre_mapping.yaml', 'templates/*.html'],
     },
     install_requires=[
         "redis",
